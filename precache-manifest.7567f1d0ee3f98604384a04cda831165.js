9bec04f11b4f228a952be271af007902989c61f8
Alen Osmanovic <alen.osmanovic@cocomore.com>
2022-11-14 15:20:06 +0100

    Updates

---
+++ precache-manifest.7567f1d0ee3f98604384a04cda831165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180383529588b384233a2bfb664fea94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "161021dba0eceff33585",
+    "url": "./static/css/main.18cf99a4.chunk.css"
+  },
+  {
+    "revision": "f2822684bd6d65380622",
+    "url": "./static/js/2.f921fc17.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "./static/js/2.f921fc17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161021dba0eceff33585",
+    "url": "./static/js/main.14c2be43.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.14c2be43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bb68dc332ff91120f6",
+    "url": "./static/js/runtime-main.b0fc149f.js"
+  }
+]);
